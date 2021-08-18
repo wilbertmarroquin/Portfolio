6ce48d256eb596c09a21d2a46f7db3ac95da00fd
WilbertMarroquin <wilbert.marroquin@dekoding.pe>
2021-08-18 17:46:41 -0500

    Clean icons from work card to go directly to work url

--- src/components/WorkCard/WorkContent/index.jsx
+++ src/components/WorkCard/WorkContent/index.jsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Container, Row } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faLink } from '@fortawesome/free-solid-svg-icons';
 import routesDictionary from '../../../routes/routesDict';
 
 import './styles.scss';
 
 const WorkContent = (props) => {
   const {
-    title, description, id, pageLink,
+    title, description, id,
   } = props;
+  const history = useHistory();
 
   return (
-    <Container>
+    <Container onClick={() => { history.push(`${routesDictionary.work}/${id}`); }}>
       <Row className="card-title">
         <Link to={`${routesDictionary.work}/${id}`}>{ title }</Link>
       </Row>
       <Row>
         {description}
       </Row>
-      <Link className="card-search" to={`${routesDictionary.work}/${id}`}>
-        <FontAwesomeIcon icon={faSearch} size="lg" />
-      </Link>
-      <a className="card-link" href={pageLink} target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faLink} size="lg" />
-      </a>
     </Container>
   );
 };
